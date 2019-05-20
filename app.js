a21aa523df53c67cebbed2b19367d291bc8ccd6a
pavan <pavanammula@gmail.com>
2019-05-20 22:04:05 +0530

    created an arrow function

---
+++ app.js
@@ -0,0 +1,13 @@
+//without return
+var addition = (a,b) => a+b;
+console.log(addition(5,1));
+//with return
+var addition = (a,b) =>{
+    return a+b;
+}
+console.log(addition(9,8));
+//Without Arrow function
+var addition = function(a,b){
+    return a+b;
+}
+console.log(addition(10,9));
